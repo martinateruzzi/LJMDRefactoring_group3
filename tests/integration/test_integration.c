0dfbdf5be62ac6ecec6ddefee4220ee2ef082482
Martina Teruzzi <teruzzi.martina@gmail.com>
2020-02-13 15:34:27 +0100

    Makefile for tests

---
+++ tests/integration/test_integration.c
@@ -0,0 +1,124 @@
+/* 
+ * simple lennard-jones potential MD code with velocity verlet.
+ * units: Length=Angstrom, Mass=amu; Energy=kcal
+ *
+ * baseline c version.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "prototypes.h"
+
+/* main */
+int main(int argc, char **argv) 
+{
+    int nprint, i;
+    char restfile[200], trajfile[200], ergfile[200], line[200];
+    FILE *fp,*traj,*erg;
+    mdsys_t sys;
+
+    /* read input file */
+    if(get_a_line(stdin,line)) return 1;
+    sys.natoms=atoi(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.mass=atof(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.epsilon=atof(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.sigma=atof(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.rcut=atof(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.box=atof(line);
+    if(get_a_line(stdin,restfile)) return 1;
+    if(get_a_line(stdin,trajfile)) return 1;
+    if(get_a_line(stdin,ergfile)) return 1;
+    if(get_a_line(stdin,line)) return 1;
+    sys.nsteps=atoi(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.dt=atof(line);
+    if(get_a_line(stdin,line)) return 1;
+    nprint=atoi(line);
+
+    /* allocate memory */
+    sys.rx=(double *)malloc(sys.natoms*sizeof(double));
+    sys.ry=(double *)malloc(sys.natoms*sizeof(double));
+    sys.rz=(double *)malloc(sys.natoms*sizeof(double));
+    sys.vx=(double *)malloc(sys.natoms*sizeof(double));
+    sys.vy=(double *)malloc(sys.natoms*sizeof(double));
+    sys.vz=(double *)malloc(sys.natoms*sizeof(double));
+    sys.fx=(double *)malloc(sys.natoms*sizeof(double));
+    sys.fy=(double *)malloc(sys.natoms*sizeof(double));
+    sys.fz=(double *)malloc(sys.natoms*sizeof(double));
+
+    /* read restart */
+    fp=fopen(restfile,"r");
+    if(fp) {
+        for (i=0; i<sys.natoms; ++i) {
+            fscanf(fp,"%lf%lf%lf",sys.rx+i, sys.ry+i, sys.rz+i);
+        }
+        for (i=0; i<sys.natoms; ++i) {
+            fscanf(fp,"%lf%lf%lf",sys.vx+i, sys.vy+i, sys.vz+i);
+        }
+        fclose(fp);
+        azzero(sys.fx, sys.natoms);
+        azzero(sys.fy, sys.natoms);
+        azzero(sys.fz, sys.natoms);
+    } else {
+        perror("cannot read restart file");
+        return 3;
+    }
+
+    /* initialize forces and energies.*/
+    sys.nfi=0;
+
+    update_velocities_positions(&sys);
+
+	/* setting forces manually */
+	for (i=0; i<sys.natoms; ++i) {
+		sys.fx[i] = i*10e4-0.5;	
+		sys.fy[i] = -i*10e4+0.5;	
+		sys.fz[i] = i*10e4+0.5;	
+	}
+    
+	update_velocities(&sys);
+
+	
+
+    //force(&sys);
+    //ekin(&sys);
+    
+    erg=fopen(ergfile,"w");
+    traj=fopen(trajfile,"w");
+
+	for (i=0; i<sys.natoms; ++i) {
+		fprintf(erg, "Ar  %20.8f %20.8f %20.8f\n", sys.rx[i], sys.ry[i], sys.rz[i]);
+	}
+
+	fprintf(erg, "\n");
+
+	for (i=0; i<sys.natoms; ++i) {
+		fprintf(erg, "Ar  %20.8f %20.8f %20.8f\n", sys.vx[i], sys.vy[i], sys.vz[i]);
+	}
+
+    /* clean up: close files, free memory */
+    printf("Test integration Done.\n");
+    fclose(erg);
+    fclose(traj);
+
+    free(sys.rx);
+    free(sys.ry);
+    free(sys.rz);
+    free(sys.vx);
+    free(sys.vy);
+    free(sys.vz);
+    free(sys.fx);
+    free(sys.fy);
+    free(sys.fz);
+
+    return 0;
+}
