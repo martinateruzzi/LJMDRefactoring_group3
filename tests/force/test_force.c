2e8939fc48d64706ac0258b80d317262f1edd68e
saliei <saeidaliei2019@gmail.com>
2020-02-13 23:16:35 +0100

    dont calc unnecessary parts

--- tests/force/test_force.c
+++ tests/force/test_force.c
@@ -8,8 +8,8 @@
 int main(int argc, char **argv) 
 {
     int nprint, i;
-    char restfile[BLEN], trajfile[BLEN], ergfile[BLEN], line[BLEN];
-    FILE *fp,*traj,*erg;
+    char restfile[BLEN], trajfile[BLEN], ergfile[BLEN], line[BLEN], frcfile[BLEN];
+    FILE *fp, *trj, *erg, *frc;
     mdsys_t sys;
 
     /* read input file */
@@ -28,6 +28,7 @@ int main(int argc, char **argv)
     if(get_a_line(stdin,restfile)) return 1;
     if(get_a_line(stdin,trajfile)) return 1;
     if(get_a_line(stdin,ergfile)) return 1;
+    if(get_a_line(stdin,frcfile)) return 1;
     if(get_a_line(stdin,line)) return 1;
     sys.nsteps=atoi(line);
     if(get_a_line(stdin,line)) return 1;
@@ -68,22 +69,21 @@ int main(int argc, char **argv)
     sys.nfi=0;
     force(&sys);
     ekin(&sys);
-    
-    erg=fopen(ergfile,"w");
-    traj=fopen(trajfile,"w");
-
-    printf("Starting simulation with %d atoms for %d steps.\n",sys.natoms, sys.nsteps);
-    printf("     NFI            TEMP            EKIN                 EPOT              ETOT\n");
-    output(&sys, erg, traj);
+    frc=fopen(frcfile, "w");
 
     /**************************************************/
     /* main MD loop */
     for(sys.nfi=1; sys.nfi <= sys.nsteps; ++sys.nfi) {
 
         /* write output, if requested */
-        if ((sys.nfi % nprint) == 0)
-            output(&sys, erg, traj);
-
+        if ((sys.nfi % nprint) == 0){
+            /* write force to file if requested */
+            for(i=0; i < sys.natoms; ++i)
+            {
+                printf("%.3f, %.3f, %.3f\n", sys.fx[i], sys.fy[i], sys.fz[i]);
+                fprintf(frc, "%.3f, %.3f, %.3f\n", sys.fx[i], sys.fy[i], sys.fz[i]);
+            }
+        }
         /* propagate system and recompute energies */
         velverlet(&sys);
         ekin(&sys);
@@ -91,9 +91,8 @@ int main(int argc, char **argv)
     /**************************************************/
 
     /* clean up: close files, free memory */
-    printf("Simulation Done.\n");
-    fclose(erg);
-    fclose(traj);
+    printf("Force have be calculated!\n");
+    fclose(frc);
 
     free(sys.rx);
     free(sys.ry);
