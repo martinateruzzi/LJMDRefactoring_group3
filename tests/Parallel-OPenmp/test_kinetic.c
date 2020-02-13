7f47ca549e54fb28c9b3a25ece2d65d394577df5
NaimaNess <nyousfi@ictp.it>
2020-02-13 16:27:26 +0100

    final serial kinetic test

--- tests/Parallel-OPenmp/test_kinetic.c
+++ tests/Parallel-OPenmp/test_kinetic.c
@@ -16,21 +16,34 @@ int main(int argc, char **argv)
     FILE *fp,*traj,*erg;
     mdsys_t sys;
 
-
-
-    /* read input file */
     if(get_a_line(stdin,line)) return 1;
     sys.natoms=atoi(line);
-
     if(get_a_line(stdin,line)) return 1;
     sys.mass=atof(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.epsilon=atof(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.sigma=atof(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.rcut=atof(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.box=atof(line);
+    if(get_a_line(stdin,restfile)) return 1;
+    if(get_a_line(stdin,trajfile)) return 1;
+    if(get_a_line(stdin,ergfile)) return 1;
+    if(get_a_line(stdin,line)) return 1;
+    sys.nsteps=atoi(line);
+    if(get_a_line(stdin,line)) return 1;
+    sys.dt=atof(line);
+    if(get_a_line(stdin,line)) return 1;
+    nprint=atoi(line);
+
 
     sys.vx=(double *)malloc(sys.natoms*sizeof(double));
     sys.vy=(double *)malloc(sys.natoms*sizeof(double));
     sys.vz=(double *)malloc(sys.natoms*sizeof(double));
     
-    /* read restart */
-     
+       
     fp=fopen(restfile,"r");
     if(fp) {
         for (i=0; i<sys.natoms; ++i) {
