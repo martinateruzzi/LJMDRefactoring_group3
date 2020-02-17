018e874dfd0e3e7662c70a3bdd58baba37eed29b
Gianluca <gianluca.coidessa@yahoo.it>
2020-02-17 18:53:57 +0100

    adding mpi

--- src/force.c
+++ src/force.c
@@ -16,53 +16,64 @@
 /* helper function: apply minimum image convention */
 static double pbc(double x, const double boxby2)
 {
-    while (x >  boxby2) x -= 2.0*boxby2;
-    while (x < -boxby2) x += 2.0*boxby2;
-    return x;
+  while (x >  boxby2) x -= 2.0*boxby2;
+  while (x < -boxby2) x += 2.0*boxby2;
+  return x;
 }
 
 /* compute forces */
 void force(mdsys_t *sys) 
 {
-    double ffac, sigma6, c6, c12, rcsq, rsq;
-    double rx,ry,rz;
-    int i,j;
+  double ffac, sigma6, c6, c12, rcsq, rsq;
+  double rx,ry,rz;
+  int i,j,ii;
 
-    /* zero energy and forces */
-    sys->epot=0.0;
-    azzero(sys->fx,sys->natoms);
-    azzero(sys->fy,sys->natoms);
-    azzero(sys->fz,sys->natoms);
+  /* zero energy and forces */
+  double epot=0.0;
+  azzero(sys->cx,sys->natoms);
+  azzero(sys->cy,sys->natoms);
+  azzero(sys->cz,sys->natoms);
 
-    sigma6 = sys->sigma*sys->sigma*sys->sigma*sys->sigma*sys->sigma*sys->sigma;
-    c6 =4.0*sys->epsilon*sigma6;
-    c12 =4.0*sys->epsilon*sigma6*sigma6;
-    rcsq = sys->rcut * sys->rcut;
+  sigma6 = sys->sigma*sys->sigma*sys->sigma*sys->sigma*sys->sigma*sys->sigma;
+  c6 =4.0*sys->epsilon*sigma6;
+  c12 =4.0*sys->epsilon*sigma6*sigma6;
+  rcsq = sys->rcut * sys->rcut;
+    
+  MPI_Bcast(sys->rx, sys->natoms, MPI_DOUBLE, 0, sys->mpicomm);
+  MPI_Bcast(sys->ry, sys->natoms, MPI_DOUBLE, 0, sys->mpicomm);
+  MPI_Bcast(sys->rz, sys->natoms, MPI_DOUBLE, 0, sys->mpicomm);
 
-    for(i=0; i < (sys->natoms)-1 ; ++i) {
-        for(j=i+1; j < (sys->natoms); ++j) {
+  for(i=0; i < (sys->natoms)-1 ; i+=sys->nsize) {
+    ii=i+sys->mpirank;
+    if (ii >= (sys->natoms - 1)) break;
+    for(j=i+1; j < (sys->natoms); ++j) {
 
-            /* get distance between particle i and j */
-            rx=pbc(sys->rx[i] - sys->rx[j], 0.5*sys->box);
-            ry=pbc(sys->ry[i] - sys->ry[j], 0.5*sys->box);
-            rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);
-            rsq = rx*rx + ry*ry + rz*rz;
+      /* get distance between particle i and j */
+      rx=pbc(sys->rx[i] - sys->rx[j], 0.5*sys->box);
+      ry=pbc(sys->ry[i] - sys->ry[j], 0.5*sys->box);
+      rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);
+      rsq = rx*rx + ry*ry + rz*rz;
       
-            /* compute force and energy if within cutoff */
-            if (rsq < rcsq) {
+      /* compute force and energy if within cutoff */
+      if (rsq < rcsq) {
 
-		double r6,rinv; rinv=1.0/rsq; r6=rinv*rinv*rinv;
-		ffac = (12.0*c12*r6 - 6.0*c6)*r6*rinv;
-		sys->epot += r6*(c12*r6 - c6);
+	double r6,rinv; rinv=1.0/rsq; r6=rinv*rinv*rinv;
+	ffac = (12.0*c12*r6 - 6.0*c6)*r6*rinv;
+	epot += r6*(c12*r6 - c6); /*change epot is fo every prcessor, then we will do the reduce*/
 
-                sys->fx[i] += rx*ffac;
-                sys->fy[i] += ry*ffac;
-                sys->fz[i] += rz*ffac;
+	sys->cx[i] += rx*ffac;
+	sys->cy[i] += ry*ffac;
+	sys->cz[i] += rz*ffac;
 
-                sys->fx[j] -= rx*ffac;
-                sys->fy[j] -= ry*ffac;
-                sys->fz[j] -= rz*ffac;
-            }
-        }
+	sys->cx[j] -= rx*ffac;
+	sys->cy[j] -= ry*ffac;
+	sys->cz[j] -= rz*ffac;
+      }
     }
+  }
+    MPI_Reduce(sys->cx, sys->fx, sys->natoms, MPI_DOUBLE, MPI_SUM, 0, sys->mpicomm);
+    MPI_Reduce(sys->cy, sys->fy, sys->natoms, MPI_DOUBLE, MPI_SUM, 0, sys->mpicomm);
+    MPI_Reduce(sys->cz, sys->fz, sys->natoms, MPI_DOUBLE, MPI_SUM, 0, sys->mpicomm);
+    MPI_Reduce(&epot, &sys->epot, 1, MPI_DOUBLE, MPI_SUM, 0, sys->mpicomm);
+
 }
--- src/ljmd.c
+++ src/ljmd.c
@@ -10,6 +10,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <math.h>
+#include <mpi.h>
 
 #include "prototypes.h"
 
@@ -24,12 +25,19 @@
 /* main */
 int main(int argc, char **argv) 
 {
-    int nprint, i;
-    char restfile[BLEN], trajfile[BLEN], ergfile[BLEN], line[BLEN];
-    FILE *fp,*traj,*erg;
-    mdsys_t sys;
+  int nprint, i;
+  char restfile[BLEN], trajfile[BLEN], ergfile[BLEN], line[BLEN];
+  FILE *fp,*traj,*erg;
+  mdsys_t sys;
 
-    /* read input file */
+  sys.mpicomm=MPI_COMM_WORLD;
+  MPI_Init(&argc,&argv);
+  MPI_Comm_rank(sys.mpicomm,&sys.mpirank);
+  MPI_Comm_size(sys.mpicomm,&sys.nprocs);
+ 
+
+  /* read input file */
+  if(sys.mpirank==0){
     if(get_a_line(stdin,line)) return 1;
     sys.natoms=atoi(line);
     if(get_a_line(stdin,line)) return 1;
@@ -51,76 +59,105 @@ int main(int argc, char **argv)
     sys.dt=atof(line);
     if(get_a_line(stdin,line)) return 1;
     nprint=atoi(line);
+  }
 
-    /* allocate memory */
-    sys.rx=(double *)malloc(sys.natoms*sizeof(double));
-    sys.ry=(double *)malloc(sys.natoms*sizeof(double));
-    sys.rz=(double *)malloc(sys.natoms*sizeof(double));
-    sys.vx=(double *)malloc(sys.natoms*sizeof(double));
-    sys.vy=(double *)malloc(sys.natoms*sizeof(double));
-    sys.vz=(double *)malloc(sys.natoms*sizeof(double));
+  sys.nsize=sys.natoms/sys.nprocs;
+   
+  MPI_Bcast(&sys.dt,1,MPI_DOUBLE,0,sys.mpicomm);
+  MPI_Bcast(&sys.nsteps,1,MPI_DOUBLE,0,sys.mpicomm);
+  MPI_Bcast(&sys.box,1,MPI_DOUBLE,0,sys.mpicomm);
+  MPI_Bcast(&sys.rcut,1,MPI_DOUBLE,0,sys.mpicomm);
+  MPI_Bcast(&sys.natoms,1,MPI_DOUBLE,0,sys.mpicomm); 
+  MPI_Bcast(&sys.sigma,1,MPI_DOUBLE,0,sys.mpicomm);
+  MPI_Bcast(&sys.epsilon,1,MPI_DOUBLE,0,sys.mpicomm);
+  MPI_Bcast(&sys.mass,1,MPI_DOUBLE,0,sys.mpicomm);
+  
+  /* allocate memory */
+
+  sys.rx=(double *)malloc(sys.natoms*sizeof(double));
+  sys.ry=(double *)malloc(sys.natoms*sizeof(double));
+  sys.rz=(double *)malloc(sys.natoms*sizeof(double));
+  sys.vx=(double *)malloc(sys.natoms*sizeof(double));
+  sys.vy=(double *)malloc(sys.natoms*sizeof(double));
+  sys.vz=(double *)malloc(sys.natoms*sizeof(double));
+  /*only rank 0*/
+  if (sys.mpirank==0){
     sys.fx=(double *)malloc(sys.natoms*sizeof(double));
     sys.fy=(double *)malloc(sys.natoms*sizeof(double));
     sys.fz=(double *)malloc(sys.natoms*sizeof(double));
+  }
+  /*buffers*/
+  sys.cx=(double *)malloc(sys.natoms*sizeof(double));
+  sys.cy=(double *)malloc(sys.natoms*sizeof(double));
+  sys.cz=(double *)malloc(sys.natoms*sizeof(double));
+    
 
-    /* read restart */
+  /* read restart */
+  if (sys.mpirank==0){
     fp=fopen(restfile,"r");
     if(fp) {
-        for (i=0; i<sys.natoms; ++i) {
-            fscanf(fp,"%lf%lf%lf",sys.rx+i, sys.ry+i, sys.rz+i);
-        }
-        for (i=0; i<sys.natoms; ++i) {
-            fscanf(fp,"%lf%lf%lf",sys.vx+i, sys.vy+i, sys.vz+i);
-        }
-        fclose(fp);
-        azzero(sys.fx, sys.natoms);
-        azzero(sys.fy, sys.natoms);
-        azzero(sys.fz, sys.natoms);
+      for (i=0; i<sys.natoms; ++i) {
+	fscanf(fp,"%lf%lf%lf",sys.rx+i, sys.ry+i, sys.rz+i);
+      }
+      for (i=0; i<sys.natoms; ++i) {
+	fscanf(fp,"%lf%lf%lf",sys.vx+i, sys.vy+i, sys.vz+i);
+      }
+      fclose(fp);
+      azzero(sys.fx, sys.natoms);
+      azzero(sys.fy, sys.natoms);
+      azzero(sys.fz, sys.natoms);
     } else {
-        perror("cannot read restart file");
-        return 3;
+      perror("cannot read restart file");
+      return 3;
     }
-
-    /* initialize forces and energies.*/
-    sys.nfi=0;
-    force(&sys);
-    ekin(&sys);
-    
+  }
+  MPI_Barrier(sys.mpicomm);
+  /* initialize forces and energies.*/
+  sys.nfi=0;
+  force(&sys);
+   
+  if(sys.mpirank==0){
     erg=fopen(ergfile,"w");
     traj=fopen(trajfile,"w");
+    ekin(&sys);
 
     printf("Starting simulation with %d atoms for %d steps.\n",sys.natoms, sys.nsteps);
     printf("     NFI            TEMP            EKIN                 EPOT              ETOT\n");
     output(&sys, erg, traj);
+  }
+  /**************************************************/
+  /* main MD loop */
+  for(sys.nfi=1; sys.nfi <= sys.nsteps; ++sys.nfi) {
 
-    /**************************************************/
-    /* main MD loop */
-    for(sys.nfi=1; sys.nfi <= sys.nsteps; ++sys.nfi) {
+    /* write output, if requested */
+    if ((sys.nfi % nprint) == 0 && sys.mpirank==0)
+      output(&sys, erg, traj);
 
-        /* write output, if requested */
-        if ((sys.nfi % nprint) == 0)
-            output(&sys, erg, traj);
-
-        /* propagate system and recompute energies */
-        velverlet(&sys);
-        ekin(&sys);
+    /* propagate system and recompute energies */
+    if(sys.mpirank==0){
+      velverlet(&sys);
+      ekin(&sys);
     }
-    /**************************************************/
+  }
+  /**************************************************/
 
-    /* clean up: close files, free memory */
+  /* clean up: close files, free memory */
+  if(sys.mpirank==0){
     printf("Simulation Done.\n");
     fclose(erg);
     fclose(traj);
-
-    free(sys.rx);
-    free(sys.ry);
-    free(sys.rz);
-    free(sys.vx);
-    free(sys.vy);
-    free(sys.vz);
     free(sys.fx);
     free(sys.fy);
     free(sys.fz);
+  }
 
-    return 0;
+  free(sys.rx);
+  free(sys.ry);
+  free(sys.rz);
+  free(sys.vx);
+  free(sys.vy);
+  free(sys.vz);
+    
+  MPI_Finalize();
+  return 0;
 }
