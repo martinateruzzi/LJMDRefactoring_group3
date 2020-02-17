1cf3868b4e5c262f304f5363230e49b89cc20b62
saliei <saeidaliei2019@gmail.com>
2020-02-17 13:55:55 +0100

    add output method and resolve name conflict

--- python/ljmd.py
+++ python/ljmd.py
@@ -13,8 +13,8 @@ class Mdsys(Structure):
         ("sigma", c_double),
         ("box", c_double),
         ("rcut", c_double),
-        ("ekin", c_double),
-        ("epot", c_double),
+        ("ekinetic", c_double),
+        ("epotential", c_double),
         ("temp", c_double),
         ("rx", POINTER(c_double)),
         ("ry", POINTER(c_double)),
@@ -90,22 +90,28 @@ class Mdsys(Structure):
 
     def output(self):
         _ergstr = "{:8} {:20.8} {:20.8} {:20.8} {:20.8}\n".format(
-            self.nfi, self.temp, self.ekin, self.epot, self.ekin+self.epot)
+            self.nfi, self.temp, self.ekinetic, self.epotential, 
+            self.ekinetic+self.epotential)
         print(_ergstr)
         try:
             with open(self.ergfile, "a") as file:
                 file.write(_ergstr)
         except Exception as err:
-            print("Error reading file: {}".format(str(err)))
+            print("Error writing to file: {}".format(str(err)))
 
         try:
             with open(self.trajfile, "a") as file:
-                file.write("nfil = {:8} \t etot = {:.8}".format(
-                    self.nfi, self.ekin+self.epot))
-                
+                file.write("nfil = {:8} \t etot = {:.8}\n".format(
+                    self.nfi, self.ekinetic+self.epotential))
+                for i in range(self.natoms):
+                    _trajstr = "Ar  {:20.8} {:20.8} {:20.8}\n".format(
+                        self.rx[i], self.ry[i], self.rz[i])
+                    file.write(_trajstr)
+        except Exception as err:
+            print("Error writing to file: {}".format(str(err)))
 
     def loadengine(self):
-        self._dll = CDLL("./libljmd.so")
+        self._dll = CDLL("../libljmd.so")
         self._dll.force.argtypes = [POINTER(Mdsys)]
         self._dll.force.restype = None
         self._dll.ekin.argtypes = [POINTER(Mdsys)]
@@ -121,83 +127,3 @@ class Mdsys(Structure):
 
     def velverlet(self):
         self._dll.velverlet(byref(self))
-
-
-sys = Mdsys(initfile="../examples/argon_108.inp")
-sys.loadengine()
-
-# sys = Mdsys()
-print(sys.natoms)
-print(sys.force())
-# print(sys.ekin())
-"""
-libkinetic = CDLL("./libkinetic.so")
-ekin = libkinetic.ekin
-ekin.argtypes = [POINTER(Mdsys)]
-ekin.restype = None
-
-libforce = CDLL("./libforce.so")
-force = libforce.force
-force.argtypes = [POINTER(Mdsys)]
-force.restype = None
-
-natoms = 3
-mass = 39.948
-epsilon = 0.2379
-sigma = 3.405
-rcut = 8.5
-box = 17.1580
-rx = np.array([6.67103294321331, -10.6146871435653, 12.6336939877734], dtype=c_double)
-ry = np.array([1.06574058650169, -3.33432278188177, -2.59038677851747], dtype=c_double)
-rz = np.array([-1.78412295775301, -16.5259458407765, 4.61680014503288], dtype=c_double)
-vx = np.array([-1.5643224621482283e-03, 4.8497508563925346e-04, -4.3352481732883966e-04], dtype=c_double)
-vy = np.array([4.1676710257651452e-04, 2.2858522230176587e-05, -6.1985040462745732e-04], dtype=c_double)
-vz = np.array([-7.5611349562333923e-04, 4.0710138209103827e-04, -4.6520198934056357e-04], dtype=c_double)
-nsteps = 100
-dt = 5
-nprint = 5
-fx = np.zeros(natoms, dtype=c_double)
-fy = np.zeros(natoms, dtype=c_double)
-fz = np.zeros(natoms, dtype=c_double)
-nfi = 0
-
-sys = Mdsys()
-sys.natoms = natoms
-sys.mass = mass
-sys.epsilon = epsilon
-sys.sigma = sigma
-sys.rcut = rcut
-sys.box = box
-dblp = POINTER(c_double)
-rxp = rx.ctypes.data_as(dblp)
-ryp = ry.ctypes.data_as(dblp)
-rzp = rz.ctypes.data_as(dblp)
-vxp = vx.ctypes.data_as(dblp)
-vyp = vy.ctypes.data_as(dblp)
-vzp = vz.ctypes.data_as(dblp)
-fxp = fx.ctypes.data_as(dblp)
-fyp = fy.ctypes.data_as(dblp)
-fzp = fz.ctypes.data_as(dblp)
-sys.rx = rxp
-sys.ry = ryp
-sys.rz = rzp
-sys.vx = vxp
-sys.vy = vyp
-sys.vz = vzp
-sys.fx = fxp
-sys.fy = fyp
-sys.fz = fzp
-sys.nfi = nfi
-sys.nsteps = nsteps
-sys.dt = dt
-
-ekin(byref(sys))
-print(sys.ekin)
-print(sys.temp)
-for i in range(natoms):
-    print(sys.fx[i], sys.fy[i], sys.fz[i])
-force(byref(sys))
-for i in range(natoms):
-    print(sys.fx[i], sys.fy[i], sys.fz[i])
-nprint = int()
-"""
